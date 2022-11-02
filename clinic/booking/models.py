f44fd61b79afe7f05a5b4545d968d573d469441b
Johnkh2002 <johnkh2002@gmail.com>
2022-11-02 13:40:02 +0330

    Version 1.0

---
+++ clinic/booking/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+SERVICE_CHOICES = (
+    ("Doctor care", "Doctor care"),
+    ("Nursing care", "Nursing care"),
+    ("Medical social services", "Medical social services"),
+    ("Homemaker or basic assistance care", "Homemaker or basic assistance care"),
+    )
+TIME_CHOICES = (
+    ("3 PM", "3 PM"),
+    ("3:30 PM", "3:30 PM"),
+    ("4 PM", "4 PM"),
+    ("4:30 PM", "4:30 PM"),
+    ("5 PM", "5 PM"),
+    ("5:30 PM", "5:30 PM"),
+    ("6 PM", "6 PM"),
+    ("6:30 PM", "6:30 PM"),
+    ("7 PM", "7 PM"),
+    ("7:30 PM", "7:30 PM"),
+)
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Doctor care")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="3 PM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
